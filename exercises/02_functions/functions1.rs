a42a5edcd108e7f46f99784e8a038531bff014d0
Patrick0402 <bruno.patrick0402@gmail.com>
2025-10-24 00:30:30 -0300

    exercícios com funções

--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -1,5 +1,9 @@
 // TODO: Add some function with the name `call_me` without arguments or a return value.
 
+fn call_me(){
+    println!("Chamada da função call_me()")
+}
+
 fn main() {
     call_me(); // Don't change this line
 }
