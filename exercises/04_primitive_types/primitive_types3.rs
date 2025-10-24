e341d7a283da2aae690e813b48ace97614248e8f
Patrick0402 <bruno.patrick0402@gmail.com>
2025-10-24 01:47:11 -0300

    exercícios tipos primitivos

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -2,6 +2,35 @@ fn main() {
     // TODO: Create an array called `a` with at least 100 elements in it.
     // let a = ???
 
+    // let a = ["Array com 100 elementos"; 100];
+
+    // let a = [0; 100]; // 100 zeros
+
+    // let a = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+
+    // let a: Vec<u32> = (0..100).collect();
+
+    // let a: [u32; 100] = std::array::from_fn(|i| i as u32);
+
+    // let a: [u32; 100] = std::array::from_fn(|i| i as u32 + 1);
+
+
+    // let a: [u32; 100] = std::array::from_fn(|i| if i % 2 == 0 { 0 } else { 1 });
+    
+    let a: [u32; 100] = std::array::from_fn(|i| {
+        if i % 2 == 0 {
+            (i + 100) as u32
+        } else {
+            (i + 200) as u32
+        }
+    });
+
+    //Se a for um Vec<32> é necessário utilizar a função clone() aqui, pois esse tipo não implementa 'Copy' trait.
+    for item in a {
+        println!("{}", item);
+    }
+    //O exercício funciona se a for uma string com 100 caracteres, mas é preciso comentar esse for.
+
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
     } else {
--- exercises/04_primitive_types/primitive_types4.rs
+++ exercises/04_primitive_types/primitive_types4.rs
@@ -11,6 +11,10 @@ mod tests {
         // TODO: Get a slice called `nice_slice` out of the array `a` so that the test passes.
         // let nice_slice = ???
 
+        // let nice_slice = &a[1..4]; //Intervalo fechado (exclusivo)
+        let nice_slice = &a[1..=3]; // Intervalo aberto (inclusivo)
+
+
         assert_eq!([2, 3, 4], nice_slice);
     }
 }
